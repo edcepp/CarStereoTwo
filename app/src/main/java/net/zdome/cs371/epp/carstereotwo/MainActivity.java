2f94563a36f843502ee1ba6c7cd0897b2bbfb22a
edcepp <epp@zdome.net>
2017-01-06 19:35:49 -0800

    Got the presets and power button done

--- app/src/main/java/net/zdome/cs371/epp/carstereotwo/MainActivity.java
+++ app/src/main/java/net/zdome/cs371/epp/carstereotwo/MainActivity.java
@@ -2,29 +2,87 @@ package net.zdome.cs371.epp.carstereotwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected boolean displayOn = false;
     protected TextView radio_display;
-    protected int initial_preset = 0;
+    protected int initial_preset = 1;
     protected int active_preset = initial_preset;
     protected String[] preset_banner_list;
 
+    //------ updateDisplay
+    // Helper method updates display to any changes.
+    private void updateDisplay () {
+        radio_display.setText(preset_banner_list[active_preset-1]);
+        if (displayOn) {
+            radio_display.setTextColor(Color.GREEN);
+        } else {
+            radio_display.setTextColor(Color.BLACK);
+        }
+    }
+
+    //------ onCreate
+    // Initialize the radio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize display
-        radio_display = (TextView)findViewById(R.id.channel_display);
-        radio_display.setText(getString(R.string.default_banner));
-
         // Initialize peset banners
         active_preset = initial_preset;
         preset_banner_list = getResources().getStringArray(R.array.preset_banners);
+
+
+        // Initialize display
+        radio_display = (TextView) findViewById(R.id.channel_display);
+        updateDisplay ();
         //radio_display.setText(preset_banner_list[active_preset]);
 
 
     }
+
+    //------ togglePower
+    // Toggle to power off or on and update the display
+    //------
+    public void togglePower(View view) {
+        displayOn = !displayOn;
+        updateDisplay();
+    }
+
+    //------ selectPresets
+    // Change the station based on the preset button pressed
+    public void selectPreset1 (View view) {
+        active_preset = 1;
+        updateDisplay();
+    }
+
+    public void selectPreset2 (View view) {
+        active_preset = 2;
+        updateDisplay();
+    }
+
+    public void selectPreset3 (View view) {
+        active_preset = 3;
+        updateDisplay();
+    }
+
+    public void selectPreset4 (View view) {
+        active_preset = 4;
+        updateDisplay();
+    }
+
+    public void selectPreset5 (View view) {
+        active_preset = 5;
+        updateDisplay();
+    }
+
+    public void selectPreset6 (View view) {
+        active_preset = 6;
+        updateDisplay();
+    }
 }
+
